f9bb841e66cab1cbaa0fe05ed452854cda47381a
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-05-30 09:20:58 -0700

    Refactor with try/catch

--- scripts/scripts.mjs
+++ scripts/scripts.mjs
@@ -26,12 +26,7 @@ import { central, db1, db2, db3, vault } from "./database.mjs";
 //     }
 // }
 
-
-const dbs = {
-    db1: db1,
-    db2: db2,
-    db3: db3
-};
+const startTime = performance.now();
 
 async function userProfile(id)
 {
@@ -43,51 +38,83 @@ async function userProfile(id)
 
     let profile = {};
 
-    const userDB = await Promise.resolve(dbCheck(id));
-    console.log(userDB);
+    try {
+        profile['id'] = id;
+        const returnedDB = await central(id);
+        // console.log(returnedDB);
+        const returnedInfo = await dbs[returnedDB](id);
+        // console.log(returnedInfo);
+        const returnedPII = await vault(id);
+        // console.log(returnedPII);
+        // profile = await Promise.all([returnedInfo, returnedPII]);
+        profile = Object.assign(profile, returnedPII, returnedInfo);
+        console.log(profile);
+        return profile;
+    } catch (error) {
+        console.error(`ERROR: ${error}`);
+    }
 
-    const userInfo = await Promise.resolve(getUserInfo(userDB, id));
-    console.log(userInfo);
 
-    const userPII = await Promise.resolve(getUserPII(id));
-    console.log(userPII);
+    // console.log(profile);
+
+
+    // const userDB = await Promise.resolve(dbCheck(id));
+    // console.log(userDB);
+
+    // const userInfo = getUserInfo(userDB, id);
+    // // console.log(userInfo);
+    // // profile = Object.assign(profile, userInfo);
+
+    // const userPII =getUserPII(id);
+    // // // console.log(userPII);
+    // // profile = Object.assign(profile, userPII);
     
+    // profile = await Promise.all([userInfo, userPII]);
+    // console.log(profile);
+
     // console.log(`User DB: `)
     // console.log(userDB);
 
     // profile = await Promise.all([userDB])
     // profile['db'] = userDB;
     
-    return profile;
+    // console.log(`User Profile: `);
+    // console.log(profile);
+
+    // return profile;
 }
 
-function dbCheck(id)
-{
-    const returnedDB = central(id);
-    // console.log(`ReturnedDB: `)
-    // console.log(returnedDB);
+const endTime = performance.now();
+
+// function dbCheck(id)
+// {
+//     const returnedDB = central(id);
+//     // console.log(`ReturnedDB: `)
+//     // console.log(returnedDB);
   
-    return returnedDB;
-}
+//     return returnedDB;
+// }
 
-function getUserInfo(database, id)
-{
-    const returnedInfo = dbs[database](id);
-    // console.log(`Returned User Info: `);
-    // console.log(returnedInfo);
+// function getUserInfo(database, id)
+// {
+//     const returnedInfo = dbs[database](id);
+//     // console.log(`Returned User Info: `);
+//     // console.log(returnedInfo);
 
-    return returnedInfo;
-}
+//     return returnedInfo;
+// }
 
-function getUserPII(id)
-{
-    const returnedPII = vault(id);
-    // console.log(`Returned User PII: `);
-    // console.log(returnedPII);
+// function getUserPII(id)
+// {
+//     const returnedPII = vault(id);
+//     // console.log(`Returned User PII: `);
+//     // console.log(returnedPII);
 
-    return returnedPII;
-}
+//     return returnedPII;
+// }
 
-const profile = userProfile(1);
-// console.log(`User Profile: `);
-// console.log(profile);
+const result = userProfile(1);
+console.log(result);
+
+const elapsedMs = (endTime - startTime) * 1000;
+console.log(elapsedMs);
